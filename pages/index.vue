ad217a54a37261cc612fbbcddee9bcd1a752ea38
Matej Nemcek <ybdaba@gmail.com>
2017-07-14 10:36:28 +0200

    replacing bootstrap with bulma

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,58 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-3"><h1>👩‍💻 Decent technology.</h1></div>
-      <div class="col-lg-3"><h1>🤝 Smart Contracts.</h1></div>
-    </div>
-    <div class="row">
-      <div class="col-lg-3"><h1>🔐 Cryptography.</h1></div>
-      <div class="col-lg-3"><h1>⛓ Blockchains.</h1></div>
-    </div>
-
-    <div class="row">
-      <div class="col-12">
-        <h2 class="subtitle">
-          @NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519
-        </h2>
+  <div class="container is-fluid">
+    <div class="columns">
+      <div class="column">
+        <h1 class="title is-1">👩‍💻 Decent technology.</h1>
+      </div>
+      <div class="column">
+        <h2 class="title is-2">🤝 Smart Contracts.</h2>
+      </div>
+      <div class="column">
+        <h2 class="title is-2">🔐 Cryptography.</h2>
+      </div>
+      <div class="column">
+        <h2 class="title is-2">⛓ Blockchains.</h2>
       </div>
     </div>
-    <div class="row">
-      <div class="col-lg-2">
-        <h3>
-          Billing information
-        </h3>
+    <div class="columns">
+      <div class="column ">
+        <a class="button is-outlined is-large" href="http://hypersignal.xyz">Hire Matej for project</a>
       </div>
-      <div class="col-lg-2">
-        <p>
-          Geefu s.r.o.<br>
-          Kopčianska 10<br>
-          851 01 Bratislava<br>
-          Slovak Republic, Europe<br>
-        </p>
-      </div>
-      <div class="col-lg-3">
-        <p>
-          Company No.: 46430342<br>
-          VAT: SK2023376740<br>
-          DIC: 2023376740<br>
-        </p>
+      <div class="column">
+        <a class="button is-outlined is-large" href="http://robo4hire.com">Hire a Robot for lifetime</a>
       </div>
     </div>
-    <div class="row">
-        <div class="col-lg-3"><a href="http://hypersignal.xyz" target="_blank" class="button--green">Hire Matej for project</a></div>
-        <div class="col-lg-3"><a href="http://robo4hire.com" target="_blank" class="button--green">Hire a Robot for lifetime</a></div>
+    <div class="columns">
+      <div class="column ">
+        <h3 class="title is-3">@NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519</h3>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column ">
+        <h4 class="title is-4">Billing information</h4>
+      </div>
+      <div class="column">
+        Geefu s.r.o.<br>
+        Kopčianska 10<br>
+        851 01 Bratislava<br>
+        Slovak Republic, Europe<br>
+      </div>
+      <div class="column">
+        Company No.: 46430342<br>
+        VAT: SK2023376740<br>
+        DIC: 2023376740<br>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~components/Logo.vue'
-import BootstrapVue from 'bootstrap-vue'
-
 export default {
   components: {
-    Logo,
-    BootstrapVue
   }
 }
 </script>
 
 <style>
-.subtitle {
-    font-weight: 300;
-    font-size: 20px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-}
-
+@import '~bulma/css/bulma.css';
 </style>
