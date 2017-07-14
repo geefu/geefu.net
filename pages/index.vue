044d56e6fecaba4419e584e202f676b1d937a355
Matej Nemcek <ybdaba@gmail.com>
2017-07-14 10:55:48 +0200

    lets do it in pure black

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,56 @@
 <template>
-  <div class="container is-fluid">
-    <div class="columns">
-      <div class="column">
-        <h1 class="title is-1">👩‍💻 Decent technology.</h1>
+  <div class="container">
+    <section class="hero is-medium is-dark is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <h1 class="title is-1">👩‍💻 Decent technology.</h1>
+            </div>
+            <div class="column">
+              <h2 class="title is-2">🤝 Smart Contracts.</h2>
+            </div>
+            <div class="column">
+              <h2 class="title is-2">🔐 Cryptography.</h2>
+            </div>
+            <div class="column">
+              <h2 class="title is-2">⛓ Blockchains.</h2>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="column">
-        <h2 class="title is-2">🤝 Smart Contracts.</h2>
+      <div class="columns">
+        <div class="column is-offset-1">
+          <a class="button is-warning is-large" href="http://hypersignal.xyz">Hire Matej for project</a>
+        </div>
+        <div class="column">
+          <a class="button is-warning is-large" href="http://robo4hire.com">Hire a Robot for lifetime</a>
+        </div>
       </div>
-      <div class="column">
-        <h2 class="title is-2">🔐 Cryptography.</h2>
+      <div class="columns">
+        <div class="column is-offset-1">
+          <h3 class="title is-3">@NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519</h3>
+        </div>
       </div>
-      <div class="column">
-        <h2 class="title is-2">⛓ Blockchains.</h2>
+      <div class="columns">
+        <div class="column is-offset-1">
+          <h4 class="title is-4">Billing information</h4>
+        </div>
+        <div class="column">
+          Geefu s.r.o.<br>
+          Kopčianska 10<br>
+          851 01 Bratislava<br>
+          Slovak Republic, Europe<br>
+        </div>
+        <div class="column">
+          Company No.: 46430342<br>
+          VAT: SK2023376740<br>
+          DIC: 2023376740<br>
+        </div>
       </div>
-    </div>
-    <div class="columns">
-      <div class="column ">
-        <a class="button is-outlined is-large" href="http://hypersignal.xyz">Hire Matej for project</a>
-      </div>
-      <div class="column">
-        <a class="button is-outlined is-large" href="http://robo4hire.com">Hire a Robot for lifetime</a>
-      </div>
-    </div>
-    <div class="columns">
-      <div class="column ">
-        <h3 class="title is-3">@NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519</h3>
-      </div>
-    </div>
-    <div class="columns">
-      <div class="column ">
-        <h4 class="title is-4">Billing information</h4>
-      </div>
-      <div class="column">
-        Geefu s.r.o.<br>
-        Kopčianska 10<br>
-        851 01 Bratislava<br>
-        Slovak Republic, Europe<br>
-      </div>
-      <div class="column">
-        Company No.: 46430342<br>
-        VAT: SK2023376740<br>
-        DIC: 2023376740<br>
-      </div>
-    </div>
+    </section>
+
+
   </div>
 </template>
 
